9a2b780c1deb26535b40f2cd118b04d173675c29
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-11-21 14:35:46 -0300

    url logic

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import routes from './routes';
 
 // require('../database');
 
+// logica da url -> https://loja.panini.com.br/panini/solucoes/Busca.aspx?i=40|1624,44|2085
+// capturar os titulos -> https://loja.panini.com.br/panini/produto/Manga-<Nome do manga>-edicao-<Numero da edicao>.aspx
+
 const app = express();
 
 app.use(express.json());
